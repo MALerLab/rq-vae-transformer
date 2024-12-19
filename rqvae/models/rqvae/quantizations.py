0008cac480d2f37bfe2dddbd2989a766d5fac89e
sakemin <sakemin.jung@gmail.com>
2024-12-19 11:19:19 +0900

    edit: remove size assertion for dynamic size code decoding

--- rqvae/models/rqvae/quantizations.py
+++ rqvae/models/rqvae/quantizations.py
@@ -296,7 +296,7 @@ class RQBottleneck(nn.Module):
     
     @torch.no_grad()
     def embed_code(self, code):
-        assert code.shape[1:] == self.code_shape
+        # assert code.shape[1:] == self.code_shape
         
         code_slices = torch.chunk(code, chunks=code.shape[-1], dim=-1)
 
