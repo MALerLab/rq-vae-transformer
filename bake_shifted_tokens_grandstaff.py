a14751ed53da0610bd0be8e1a907afc7cf4055b3
sakemin <sakemin.jung@gmail.com>
2024-12-22 20:08:52 +0900

    add: annotation

--- bake_shifted_tokens_grandstaff.py
+++ bake_shifted_tokens_grandstaff.py
@@ -16,7 +16,7 @@ from rqvae.models import create_model
 
 if __name__ == "__main__":
   # model_name = "sqocremavq_16_240_gray_unfit"
-  model_name = "gsrqvae_f16_c1024_k4_unshared_128p_gray_unfit"
+  model_name = "gsrqvae_f16_c1024_k4_unshared_128p_gray_unfit" # `{ds_type(gs: grandstaff, sq: string quartet)model_type(rqvae, emavq)}_f{compression_rate}_c{codebook_size}_k{n_codebook}_{shared/unshared embedding between codbooks for RQ}_{img_height}p_{gray for grayscale}_{fit/unfit training(unfit means random cropping ds image so that the model can learn to be robust to the slight pixel shifts)}`
   config_path = list((Path("logs")/ model_name).rglob("config.yaml"))[0]
   config = OmegaConf.load(config_path)
   config = load_config(config_path)
@@ -46,23 +46,16 @@ if __name__ == "__main__":
   normalize = transforms.Normalize([0.5], [0.5])
   
   for image_path in tqdm(image_path_list):
-    save_path = image_path.parent / (image_path.stem + f":{model_name}_shifted.pt")
+    save_path = image_path.parent / (image_path.stem[:-4] + f":{model_name}_shifted.pt") # [:-4] for removing "_128" part
     save_path.parent.mkdir(parents=True, exist_ok=True)
-    # if Path(str(save_path) + ":0_0.pt").exists():
-    #   continue
     
     print("Encoding : ", image_path)
     image = PIL.Image.open(image_path).convert("L")
-    # aspect_ratio = image.width / image.height
-    # new_width = int(128 * aspect_ratio)
-    # image = image.resize((new_width, 128))
-    # resized_image_path = image_path.with_stem(image_path.stem + "_128")
-    # image.save(resized_image_path)
     image = totensor(image)
     image = normalize(image)
     
     x_y_shifted_tokens = []
-    for i in range(16):
+    for i in range(16): # pixel shifting to get 16*4 shifted tokens for a single image
       x_shifted_img = image[...,i:image.shape[-1]-15+i]
       y_shifted_imgs = []
       for j in range(4):
